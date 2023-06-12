c7b2ab8d6d0e714c6b699969611fc92dfde9867c
1Snake001 <streetstylecr3w@gmail.com>
2023-06-12 11:42:53 +0200

    union type function with narrowing has been implemented

--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,14 @@ exampleFn("test_value");
 // union type
 
 let unionVariable: string | number = 'string';
+
+function unionFn( input: string | number){
+    // narrowing
+    if(typeof input === "string"){
+        return input.length;
+    }
+   return input * 4;
+}
+
+unionFn("value");
+unionFn(5);
